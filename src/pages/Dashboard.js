3e240bac8c1971641c3c6dfaa1a1847fbf25230a
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-19 14:23:51 +0000

    feat: add header component to dashboard page and start creating the page skeleton

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { HeaderComponent, InputComponent } from '../components';
 
 export class Dashboard extends React.Component {
+  value = 'udhbdcn,rufyirbndi,ifbrcndicokudscbinediox.uriufhcn. rifyibunedjeribfcndi eubid ueibfdcn uhebdcdincb uebdcdni idbcnd. widbncsk dcbinsdj ibcnid';
   render() {
-    return <div>Hello</div>
+    return <div className="h-100">
+      <HeaderComponent />
+      <div className="d-flex h-100 px-5">
+        <div className="row w-100">
+          <div className="col-6">
+            <h5>Case description</h5>
+            <div className="w-100">
+              <InputComponent type="textarea" value={this.value} rows="20" />
+            </div>
+          </div>
+          <div className="col-6">Conditions</div>
+        </div>
+      </div>
+    </div>
   }
 }
