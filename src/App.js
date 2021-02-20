8c5265178ea7e3e59ce826378cca57f042d25171
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-20 12:58:44 +0000

    chore: update page import; feat: add flex 1 class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Dashboard } from './pages';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 class App extends React.Component {
 
   render() {
     return (
-
       <div className="pageBackground">
         <Router>
           <Route exact path="/" component={Login} />
           <Route path="/dashboard" component={Dashboard} />
         </Router>
       </div>
-
     );
   }
 }
