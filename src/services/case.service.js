a640415ea66ea9563aaf603a3fcab3d46337a688
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-20 17:39:26 +0000

    feat: add servcie to get conditions and cases

---
+++ src/services/case.service.js
@@ -0,0 +1,13 @@
+import { createHeaders } from "./utils.service";
+import axios from 'axios';
+
+export class CaseService {
+  async getOpenCasesAndConditions() {
+    const casesAndConditions = await Promise.all([
+      axios.get('http://localhost:3010/case', { headers: createHeaders(true), query: { evaluated: false } }),
+      axios.get('http://localhost:3010/condition', { headers: createHeaders(true) })
+    ]);
+
+    return { cases: casesAndConditions[0].data, conditions: casesAndConditions[1].data };
+  }
+}
