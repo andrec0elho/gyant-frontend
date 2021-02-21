c1ffc9b1997b54ec8c44875502964c7d093637f3
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-21 10:28:14 +0000

    feat: add function to update the case with the condition; fix: the get all cases to send the params;

--- src/services/case.service.js
+++ src/services/case.service.js
@@ -4,10 +4,14 @@ import axios from 'axios';
 export class CaseService {
   async getOpenCasesAndConditions() {
     const casesAndConditions = await Promise.all([
-      axios.get('http://localhost:3010/case', { headers: createHeaders(true), query: { evaluated: false } }),
+      axios.get('http://localhost:3010/case', { headers: createHeaders(true), params: { evaluated: false } }),
       axios.get('http://localhost:3010/condition', { headers: createHeaders(true) })
     ]);
 
     return { cases: casesAndConditions[0].data, conditions: casesAndConditions[1].data };
+  }
+
+  async updateCase(caseId, body) {
+    await axios.patch(`http://localhost:3010/case/${caseId.toString()}`, body, { headers: createHeaders(true) });
   }
 }
