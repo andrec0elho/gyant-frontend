91765b808c4a17a6d1dcb06e6fd39bb913cd931b
Andre Coelho <Andre.Coelho@hostelworld.com>
2021-02-21 12:32:20 +0000

    chore: add package for toasts; feat: add validations on login; refactor: change component to class component and removed some console logs;

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,69 +3,106 @@ import logo from '../assets/images/gyant-logo.svg';
 import { ButtonComponent, InputComponent } from '../components';
 import { AuthService, UserService } from '../services';
 import { setTokenStorage, setUserStorage } from '../services/utils.service';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function Login(props) {
-  const credentials = {
-    email: null,
-    password: null
-  };
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      credentials: {
+        email: null,
+        password: null
+      },
+      validators: {
+        invalidEmail: false,
+        invalidPassword: false,
+        invalidCredentials: false,
+      },
+    };
+    this.authService = new AuthService();
+    this.userService = new UserService();
+  }
 
-  const authService = new AuthService();
-  const userService = new UserService();
+  login = async () => {
+    console.log(this.state.credentials)
 
-  // handle button click of login form
-  const login = async () => {
-    console.log(credentials)
-    try {
-      const token = await authService.login(credentials);
+    if (!this.isEmailValid(this.state.credentials.email)) {
+      toast.error("Please use a valid email");
+      return;
+    }
 
-      if (!token) {
-        // TODO: Error
-      }
-      console.log(token)
-      setTokenStorage(token);
-      const user = await userService.getMyProfile();
-      console.log(user);
-      setUserStorage(user);
-    } catch (error) {
-      console.log(error)
+    if (!this.isPasswordValid(this.state.credentials.password)) {
+      toast.error("Unsecure Password. Use at least 1 lowercase, 1 uppercase, 1 number and 8 characters");
+      return;
     }
 
 
-    props.history.push('/dashboard');
+    try {
+      const token = await this.authService.login(this.state.credentials);
+      setTokenStorage(token);
+
+      const user = await this.userService.getMyProfile();
+      setUserStorage(user);
+
+      this.props.history.push('/dashboard');
+    } catch (error) {
+      toast.error("Invalid credentials");
+    }
   }
 
-  const saveEmail = (value) => {
-    console.log("---------", value)
-    credentials.email = value;
+  saveEmail = (value) => {
+    this.setState(({ credentials }) => ({ credentials: { ...credentials, email: value } }));
+  }
+
+  savePassword = (value) => {
+    this.setState(({ credentials }) => ({ credentials: { ...credentials, password: value } }));
 
   }
 
-  const savePassword = (value) => {
-    credentials.password = value;
+  isEmailValid = (email) => {
+    if (!email) {
+      return false;
+    }
+
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const isEmailValid = regex.test(email.toLowerCase());
+    this.setState(({ validators }) => ({ validators: { ...validators, invalidEmail: !isEmailValid } }));
+    return isEmailValid;
   }
 
-  return (
-    <div className="row justify-content-center">
-      <div className="col-12 col-lg-6 col-xl-3">
-        <div>
-          <img src={logo} className="mw-100" alt="logo" />
-        </div>
+  isPasswordValid = (password) => {
+    if (!password) {
+      return false;
+    }
 
-        <div className="w-100 panados">
-          <div className="pb-1rem">
-            <InputComponent name="loginEmail" type="text" label="Email" onChange={saveEmail} />
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password);
+    this.setState(({ validators }) => ({ validators: { ...validators, invalidPassword: !isPasswordValid } }));
+    return isPasswordValid;
+  }
+
+  render() {
+    return (
+      <div className="row justify-content-center" >
+        <div className="col-8 col-md-6 col-lg-4 col-xl-3">
+          <div>
+            <img src={logo} className="mw-100" alt="logo" />
           </div>
-          <div className="pb-1rem">
-            <InputComponent name="loginPassword" type="password" label="Password" onChange={savePassword} />
+
+          <div className="w-100">
+            <div className="pb-1rem">
+              <InputComponent name="loginEmail" type="text" label="Email" onChange={this.saveEmail} />
+            </div>
+            <div className="pb-1rem">
+              <InputComponent name="loginPassword" type="password" label="Password" onChange={this.savePassword} />
+            </div>
+            <ToastContainer position="bottom-center" />
+            <ButtonComponent buttonClick={this.login} label={"Login"} disabled={!this.state.credentials.email && !this.state.credentials.password} />
           </div>
-          <ButtonComponent buttonClick={login} label={"Login"} />
         </div>
-
-
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Login;
